d631b67a03b83f0d164ff10e219b35a3a0c21937
Trinh-Quoc-Trong <rtytgb2423@gmail.com>
2025-03-06 12:12:46 +0700

    chip ESP32 S Class

---
+++ example203_test_001/main/hello_world_main.c
@@ -0,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+TaskHandle_t HelloWorldTaskHandle = NULL;
+void HelloWorld_Task(void *arg)
+{
+while (1)
+{
+printf("Task running: Hello World..\n");
+vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
+}
+void app_main(void)
+{
+xTaskCreate(HelloWorld_Task, "HelloWorld", 4096, NULL, 10, & HelloWorldTaskHandle);
+//xTaskCreatePinnedToCore (HelloWorld_Task, "HelloWorld", 4096, NULL, 10, &HelloWorldTaskHandle, 1); // Run on Core 1
+}
